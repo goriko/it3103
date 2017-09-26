510811456b9f693c9d3429b5f8a6f5e8c6d6fe14
goriko <guanzonedrian@gmail.com>
2017-09-26 21:34:21 +0800

    again

--- application/views/Customer_form.php
+++ application/views/Customer_form.php
@@ -2,52 +2,49 @@
     $this->session->userdata['logged_in']['nav'] = 1;
 ?>
 <html>
-<meta charset ="utf-8">
+  <head>
+    <meta charset ="utf-8">
     <link rel ="stylesheet" href="<?php echo base_url("css/dataTables.min.css"); ?>">
     <link rel ="stylesheet" href="<?php echo base_url("css/bootstrap.min.css"); ?>">
     <link rel ="stylesheet" href="<?php echo base_url("css/style.css"); ?>">
     <link rel ="stylesheet" href="<?php echo base_url("css/font-awesome.min.css"); ?>">
     <link rel ="stylesheet" href="<?php echo base_url("css/font-awesome.css"); ?>">
     <link rel ="stylesheet" href="<?php echo base_url("css/cars.css"); ?>">
-<head>
+  </head>
 	<body>
     <?php include('navbar.php');?>
     <br/>
 		<div class="col-md-offset-1 col-md-10 ordertable">
-		<div class="col-md-12 ordertable">
         <!--Customer Table-->
-        	<center>
-            	<h1>Customer Table</h1>
-                	<table class ="customerTable">
-                    	<thead>
-                        	<th>Customer ID</th>
-                            <th>Customer Name</th>
-                            <th>Civil Status</th>
-                            <th>Address</th>
-                            <th>Contact Number</th>
-                            <th></th>
-                        </thead>
-                    	<tbody>
-                    		<?php
-                    		foreach($customers as $r) {
-                    		echo"<tr>
-                    		<td> $r->CustomerID</td>
-                    		<td> $r->Name</td>
-                    		<td> $r->CivilStatus </td>
-                    		<td> $r->Address </td>
-                    		<td> $r->ContactNumber </td>
-                            <td> <button class='btn btn-success'>Edit</button>";?>
-                            <a href="<?php echo base_url("index.php/Transaction_controller/index/".$r->CustomerID);?>"><button class='btn btn-warning'>Transaction</td></a>
-                            <?php
-                            echo "</tr>";
-                                }
-                    		?>
-                 		</tbody>
-                    </table>
-            </center>
-         </div>
-	   </body>
-    </head>
+        <center>
+          <h1>Customer Table</h1>
+          <table class ="customerTable">
+            <thead>
+              <th>Customer ID</th>
+              <th>Customer Name</th>
+              <th>Civil Status</th>
+              <th>Address</th>
+              <th>Contact Number</th>
+              <th></th>
+            </thead>
+            <tbody>
+              <?php
+                foreach($customers as $r) {
+                echo"<tr>
+                  <td> $r->CustomerID</td>
+                  <td> $r->Name</td>
+                  <td> $r->CivilStatus </td>
+                  <td> $r->Address </td>
+                  <td> $r->ContactNumber </td>
+                  <td> <button class='btn btn-success'>Edit</button>";?>
+              <a href="<?php echo base_url("index.php/Transaction_controller/index/".$r->CustomerID);?>"><button class='btn btn-warning'>Transaction</td></a>
+              <?php echo "</tr>"; }?>
+            </tbody>
+          </table>
+        </center>
+      </div>
+    </body>
+  </head>
 </html>
 <script src="<?php echo base_url(); ?>js/jquery.min.js"></script>
 <script src="<?php echo base_url(); ?>js/datatables.min.js"></script>
--- application/views/navbar.php
+++ application/views/navbar.php
@@ -1,45 +1,42 @@
 <link  rel="stylesheet" href="<?php echo base_url('assests/bootstrap/css/bootstrap.min.css')?>">
-<div class="row">
+	<div class="row">
 		<div class="topbar">
-     		<div class="col-md-1">
-     			<img class="logo" src="<?php echo base_url('img/kiasmall.png')?>">
-     		</div>
+ 			<div class="col-md-1">
+     		<img class="logo" src="<?php echo base_url('img/kiasmall.png')?>">
+     	</div>
 
-        <div class="topblocks">
-          <div class="topblock topfirst">
-	     		    <a href="<?php echo base_url('index.php/Customer_controller')?>" <?php if($this->session->userdata['logged_in']['nav'] == 1){
-	     		    		echo "class='link active'";
-	     		    	}else{
-	     		    		echo "class='link'";
-	     		    		}?>>
+      <div class="topblocks">
+        <div class="topblock topfirst">
+	        <a href="<?php echo base_url('index.php/Customer_controller')?>" <?php if($this->session->userdata['logged_in']['nav'] == 1){
+	       			echo "class='link active'";
+	     			}else{
+	     		   	echo "class='link'";
+	     			}?>>
+	    		<div>
+      			<span><i class="glyphicon glyphicon-user icon">Customers</i></span>
+	   	  	</div>
+	      </a>
+	     </div>
 
-
-	     		    
-	     		    <div>
-	     				      <span><i class="glyphicon glyphicon-user icon">Customers</i></span>
-	     			  </div>
-	     		    </a>
-	     		</div>
-
-          <div class="topblock">
-	     		    <a href="<?php echo base_url("index.php/Carcontroller2");?>" <?php if($this->session->userdata['logged_in']['nav'] == 2){
-	     		    		echo "class='link active'";
-	     		    	}else{
-	     		    		echo "class='link'";
-	     		    		}?>>
-	     			  <div>
-	     				      <span ><i class="fa fa-car icon">Cars</i></span>
-	     			  </div>
-	     		    </a>
-	     		</div>
-
-	     		<div class="topblock logout col-md-offset-11">
-	     		<a href="<?php echo base_url("index.php/Logout_controller/logout");?>" class="linkout">
-	     			<div>
-	     				<span class="fa fa-sign-out"></span><span>Log Out</span>
-	     			</div>
-	     		</a>
-	     		</div>
+      <div class="topblock">
+	    	<a href="<?php echo base_url("index.php/Carcontroller2");?>" <?php if($this->session->userdata['logged_in']['nav'] == 2){
+	     	  		echo "class='link active'";
+	     	  	}else{
+	     	   		echo "class='link'";
+	     	 		}?>>
+	     	<div>
+	     		<span ><i class="fa fa-car icon">Cars</i></span>
 	     	</div>
-      </div>
+	     </a>
+		 </div>
+
+	   <div class="topblock logout col-md-offset-11">
+	   	<a href="<?php echo base_url("index.php/Logout_controller/logout");?>" class="linkout">
+	   		<div>
+	   			<span class="fa fa-sign-out"></span><span>Log Out</span>
+	     	</div>
+	    </a>
+	   </div>
+	  </div>
+	</div>
 </div>
