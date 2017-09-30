4b2267091c8329d1da5ebd1461d8eb772a21b72c
goriko <guanzonedrian@gmail.com>
2017-09-30 22:26:44 +0800

    asdas

--- application/views/Neworderview.php
+++ application/views/Neworderview.php
@@ -33,57 +33,87 @@ $user=$this->session->userdata['logged_in']['fname'];
           ?>
         </select><br><br>
 
-              <span>Mode of Payment</span>
-              <span>
-                <select name="paymentmode" class="infoput">
-                  <option value="down">Downpayment</option>
-                  <option value="full">Full Payment</option>
-                </select>
-              </span><br><br>
+        <span>Mode of Payment</span>
+        <span>
+          <select name="paymentmode" class="infoput">
+            <option value="down">Downpayment</option>
+            <option value="full">Full Payment</option>
+          </select>
+        </span><br><br>
 
-              <div id="downamount">
-              <span>Select terms:</span>
-              <span>
-                <select id="paymentt" name="term" class="infoput">
-                  <option value="12">12 mos.</option>
-                  <option value="24">24 mos.</option>
-                </select>
-              </span><br><br>
-
-
-
-              </div>
-            <button class="btn btn-primary" onclick="myFunction()">Buy!</button>
-              </form>
-              </center>
-      <span>If customer doesnt have a record yet, please click <a href="">here</a></span><br/>
-
-    </div>
-
-
-    <div class="modal fade" id="modal_form" role="dialog">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h3 class="modal-title">User Form</h3>
+        <div id="downamount">
+          <span>Select terms:</span>
+          <span>
+          <select id="paymentt" name="term" class="infoput">
+            <option value="12">12 mos.</option>
+            <option value="24">24 mos.</option>
+          </select>
+          </span><br><br>
         </div>
-        <div class="modal-body form" >
-          <form action="#" id="form" class="form-horizontal">
-            <div class="form-group" id="name">
-              <label class="control-label col-md-3">Name:</label>
-              <div class="col-md-9">
-                <input name="variant" placeholder="Variant" class="form-control" type="text" required="required" >
-              </div>
-            </div>
-          </form>
-            </div>
-            <div class="modal-footer">
-              <button type="button" id="btnSave" onclick="save()" class="btn btn-primary">Save</button>
-              <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
-            </div>
-          </div><!-- /.modal-content -->
-        </div><!-- /.modal-dialog -->
-      </div>
+
+        <button class="btn btn-primary" onclick="myFunction()">Buy!</button>
+      </form>
+      </center>
+      <span>If customer doesnt have a record yet, please click <a href='#' id='link'>here</a></span><br/>
   </body>
-  </html>
+</html>
+
+<script src="<?php echo base_url('assests/jquery/jquery-3.1.0.min.js')?>"></script>
+<script src="<?php echo base_url('assests/bootstrap/js/bootstrap.min.js')?>"></script>
+<script src="<?php echo base_url('assests/datatables/js/jquery.dataTables.min.js')?>"></script>
+<script src="<?php echo base_url('assests/datatables/js/dataTables.bootstrap.js')?>"></script>
+
+
+<script type="text/javascript">
+
+$(document).ready( function () {
+  var a = document.getElementById("link");
+        a.onclick = function() {
+          $('#form')[0].reset();
+          $('#modal_form').modal('show');
+        }
+} );
+
+</script>
+
+<div class="modal fade" id="modal_form" role="dialog">
+<div class="modal-dialog">
+  <div class="modal-content">
+    <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      <h3 class="modal-title">Add Car Form</h3>
+    </div>
+    <div class="modal-body form" >
+      <form action="#" id="form" class="form-horizontal">
+          <label class="control-label col-md-3">Name</label>
+          <div class="col-md-9">
+            <input name="name" placeholder="Name" class="form-control" type="text" required="required" >
+          </div>
+
+          <label class="control-label col-md-3">Civil Status</label>
+          <div class="col-md-9">
+              <select name='civilstatus' class='form-control'>s
+                <option value="Single">Single</option>
+                <option value="Married">Married</option>
+                <option value="Widowed">Widowed</option>
+              </select>
+          </div>
+
+          <label class="control-label col-md-3">Address</label>
+          <div class="col-md-9">
+            <input name="address" placeholder="Address" class="form-control" type="text" required="required" >
+          </div>
+
+          <label class="control-label col-md-3">Contact Number</label>
+          <div class="col-md-9">
+            <input name="contactnum" placeholder="Contact Number" class="form-control" type="text" required="required" >
+          </div>
+      </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" id="btnSave" onclick="" class="btn btn-primary">Add</button>
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
